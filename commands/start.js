f390f86d3f26584c62e54f7a79bb427404cac522
Mats Fridberg <mats.fridberg@gmail.com>
2021-03-29 19:58:16 +0200

    Move song logic
    Moving song logic to seperate file and added emojis for right/wrong answers

--- commands/start.js
+++ commands/start.js
@@ -1,6 +1,5 @@
 const { MessageEmbed } = require('discord.js');
-const ytdl = require('ytdl-core');
-const axios = require('axios').default;
+const songHandler = require('../song-handler')
 
 module.exports = {
     name: 'start',
@@ -12,81 +11,10 @@ module.exports = {
                 .then(connection => {
                     startMessage(message);
                     const playlistID = args[0];
-                    getRandomVideo(playlistID).then((videoID) =>{
-                        getMediaData(videoID).then((mediaData)=>{
-                            playSound(mediaData[2], connection);
-                            const titleFormat = /(?<=- ).+?(?= \()/;
-                            let tempTitle = mediaData[1];
-                            tempTitle = tempTitle.includes("(") 
-                                ? tempTitle.split("-")[1].split("(")[0].replace(/ /g,"").replace("'", "").toLowerCase()
-                                : tempTitle.split("-")[1].replace(/ /g,"").replace("'", "").toLowerCase();
-                            songTitle = tempTitle;
-                            console.log(`artist: ${mediaData[0]} and title: ${songTitle}`);
-                        })
-                    });
+                    songHandler.startSong(playlistID, connection)
                 });
         }
     },
-};
-
-async function playSound(videoID, connection){
-    const stream = ytdl(`https://www.youtube.com/watch?v=${videoID}`, {filter: 'audioonly'});
-    const dispatcher = connection.play(stream);
-
-    dispatcher.on('start', () => {
-        console.log('Audio is now playing!');
-    });
-
-    dispatcher.on('finish', () => {
-        console.log('Audio has finished playing!');
-    });
-
-    dispatcher.on('error', console.error);
-}
-
-async function getRandomVideo(playlistID) {
-    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistID}&key=${process.env.YTTOKEN}`;
-    let response;
-    try{response = await axios.get(url)}
-    catch(e) {console.log(e)}
-
-    const max = response.data.pageInfo.totalResults;
-    const index = Math.floor(Math.random() * parseInt(max));
-
-    const page = Math.floor(index / 50);
-    let pageToken;
-    if (response.data.nextPageToken != undefined) {
-        pageToken = response.data.nextPageToken;
-    }
-
-    for (let i = 0; i < page; i++) {
-        const nextPageUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&pageToken=${pageToken}&playlistId=${playlistID}&key=${process.env.YTTOKEN}`;
-        try{response = await axios.get(nextPageUrl);}
-        catch(e) {console.log(e);}
-        if(response.data.nextPageToken != undefined) {
-            pageToken = response.data.nextPageToken;
-        }
-    }
-    const videoPageIndex = index % 50;
-    const videoID = response.data.items[videoPageIndex].id;
-
-    return videoID;
-}
-
-async function getMediaData (videoURL) {
-    const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&id=${videoURL}&fields=items(snippet(videoOwnerChannelTitle%2C%20title%2C%20resourceId))&key=${process.env.YTTOKEN}`
-    
-    let response;
-
-    try{response = await axios.get(url)}
-    catch(e){console.log(e)}
-
-    const videoID = response.data.items[0].snippet.resourceId.videoId;
-    const uploader = response.data.items[0].snippet.videoOwnerChannelTitle;
-    const artist = uploader.substring(0, uploader.length-4);
-    const songTitle = response.data.items[0].snippet.title;
-    const mediaData = [artist, songTitle, videoID];
-    return mediaData;
 };
 
 const startMessage = (m) => {
